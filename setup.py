adb812b7231e57f3c5c7782f038d40120362fadd
Alan A.S <jhonluiz001@gmail.com>
2025-03-28 14:34:39 +0100

    folder strucutre added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='generative_ai_project',  # The name of your package
+    version='0.0.0',  # The version of your package
+    author='Alan A.s ',  # Your name or your organization’s name
+    author_email='anamthuruthilsaju@gmail.com',  # Your email
+    packages=find_packages(),  # Automatically find packages (subfolders with __init__.py)
+    install_requires=[
+        # You can list any external dependencies here
+    ],
+)
